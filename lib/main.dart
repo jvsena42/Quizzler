3070e85822a1dd9ec4f9572340aad785b8cdfda1
jvsena42 <jvsena16@gmail.com>
2021-10-16 08:32:39 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'question.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  int questionNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.questions[questionNumber].questionText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        ButtonAnswer(Colors.green, 'True', true),
+        ButtonAnswer(Colors.red, 'False', false),
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+
+  Expanded ButtonAnswer(Color color, String text, bool type) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: FlatButton(
+          textColor: Colors.white,
+          color: color,
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          ),
+          onPressed: () {
+            Question question = quizBrain.questions[questionNumber];
+            if(question.questionText == type){
+              //Correct
+            } else {
+              //wrong
+            }
+
+          },
+        ),
+      ),
+    );
+  }
+}
+
+/*
+setState(() {
+scoreKeeper.add(
+Icon(
+Icons.check,
+color: Colors.green,
+),
+);
+});*/
